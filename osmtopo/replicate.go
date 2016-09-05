66dd7932005892467a8c03695d1ae85495ecda99
Ruben Vermeersch <ruben@rocketeer.be>
2016-09-05 16:37:39 +0200

    Validate Planet checksum after downloading

--- osmtopo/replicate.go
+++ osmtopo/replicate.go
@@ -2,6 +2,7 @@ package osmtopo
 
 import (
 	"bufio"
+	"crypto/md5"
 	"errors"
 	"fmt"
 	"io"
@@ -148,27 +149,64 @@ func fetchCurrentSequence() (int64, error) {
 
 func downloadPlanet() (string, error) {
 	url := fmt.Sprintf("%s/pbf/planet-latest.osm.pbf", OsmServer)
-	return downloadProgress("planet", url)
+	file, hash, err := downloadProgress("planet", url)
+	if err != nil {
+		return "", err
+	}
+
+	hresp, err := http.Get(fmt.Sprintf("%s.md5", url))
+	if err != nil {
+		return "", err
+	}
+	defer hresp.Body.Close()
+
+	if hresp.StatusCode != 200 {
+		return "", fmt.Errorf("Unexpected status code: %d", hresp.StatusCode)
+	}
+
+	resp, err := ioutil.ReadAll(hresp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	parts := strings.Split(strings.TrimSpace(string(resp)), "  ")
+	if len(parts) != 2 {
+		return "", fmt.Errorf("Unexpected checksum file: %#v", string(resp))
+	}
+	if parts[1] != "planet-latest.osm.pbf" {
+		return "", fmt.Errorf("Unexpected filename: %s", parts[1])
+	}
+
+	if parts[0] != hash {
+		return "", fmt.Errorf("MD5 verification failed: %#v != %#v", parts[0], hash)
+	}
+
+	return file, nil
 }
 
 func downloadChangeSet(seq int64) (string, error) {
 	s := fmt.Sprintf("%09d", seq)
 	url := fmt.Sprintf("%s/replication/day/%s/%s/%s.osc.gz", OsmServer, s[0:3], s[3:6], s[6:])
-	return downloadProgress("change", url)
+	file, _, err := downloadProgress("change", url)
+	return file, err
 }
 
-func downloadProgress(tmpName, url string) (string, error) {
+func downloadProgress(tmpName, url string) (string, string, error) {
 	tmp, err := ioutil.TempFile("", tmpName)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	defer tmp.Close()
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return "", "", fmt.Errorf("Unexpected status code: %d", resp.StatusCode)
+	}
 
 	bar := pb.New(int(resp.ContentLength)).SetUnits(pb.U_BYTES)
 	bar.Start()
@@ -176,11 +214,15 @@ func downloadProgress(tmpName, url string) (string, error) {
 
 	reader := bar.NewProxyReader(resp.Body)
 
-	_, err = io.Copy(tmp, reader)
+	h := md5.New()
+	w := io.MultiWriter(tmp, h)
+
+	_, err = io.Copy(w, reader)
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	return tmp.Name(), err
+
+	return tmp.Name(), fmt.Sprintf("%02x", h.Sum(nil)), err
 }
 
 func applyChangeSet(store *Store, seq int64) error {
